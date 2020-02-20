3a0370ddc45fd23293878e17e63002ee5c0a9244
Dusan <60298038+DusannasuD@users.noreply.github.com>
2020-02-20 17:54:39 +0100

    Add files via upload

---
+++ js/videos.js
@@ -0,0 +1,14 @@
+
+// Show/hide DZ videos
+$(document).ready(function(){
+  $("#container1").click(function(){
+    $("#videos1").fadeToggle("slow");
+  });
+});
+
+// Show/hide Dusan Zaharijevic videos
+$(document).ready(function(){
+  $("#container2").click(function(){
+    $("#videos2").fadeToggle("slow");
+  });
+});
