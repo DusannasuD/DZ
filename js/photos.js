87d8b2c160947f2fbddcd31eebf58dd645b1c9e8
Dusan <60298038+DusannasuD@users.noreply.github.com>
2020-02-22 14:54:48 +0100

    Update photos.js

--- js/photos.js
+++ js/photos.js
@@ -1,8 +1,16 @@
-
+// Expand images
 function myFunction(imgs) {
   var expandImg = document.getElementById("expandedImg");
   expandImg.src = imgs.src;
   expandImg.parentElement.style.display = "block";
-  expandImg.parentElement.style.width = "70%";
+  expandImg.parentElement.style.width = "95%";
 }
 
+// Open menu
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
